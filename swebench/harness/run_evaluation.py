77aee4934bcac4479de332ad66cbbb12d307db0e
prayer <prayer@primatrix.ai>
2025-10-21 19:59:00 +0800

    not remove image

--- swebench/harness/run_evaluation.py
+++ swebench/harness/run_evaluation.py
@@ -264,8 +264,8 @@ def run_instance(
     finally:
         # Remove instance container + image, close logger
         cleanup_container(client, container, logger)
-        if rm_image:
-            remove_image(client, test_spec.instance_image_key, logger)
+        # if rm_image:
+        #     remove_image(client, test_spec.instance_image_key, logger)
         close_logger(logger)
         return {
             "completed": eval_completed,
